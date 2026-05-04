a2d1505c46b4fa76b9ed7a808b71535f95348767
ryan <ryan.brightsoft@gmail.com>
2026-05-04 13:37:15 +0700

    fix: config ios

--- ios/StudyPeak/AppDelegate.mm
+++ ios/StudyPeak/AppDelegate.mm
@@ -2,7 +2,7 @@
 // @generated begin bootsplash-header - expo prebuild (DO NOT MODIFY) sync-f0de804bd8ce3b43e3fbda77f5f6262b5152aa47
 #import "RNBootSplash.h"
 // @generated end bootsplash-header
-
+#import <Firebase.h>
 #import "OtaHotUpdate.h"
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTLinkingManager.h>
@@ -16,6 +16,7 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
+  [FIRApp configure];
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
